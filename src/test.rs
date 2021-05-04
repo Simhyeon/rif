d9d875ef1171c97257907c32714d4d177c832af0
simoncreek <simoncreek@tutanota.com>
2021-05-05 02:57:35 +0900

    Some tests experiments

--- src/test.rs
+++ src/test.rs
@@ -4,12 +4,18 @@ mod test {
     use crate::process::Writer;
     use crate::models::{ RifList ,SingleFile};
     use std::path::PathBuf;
+    use std::collections::HashMap;
 
+    #[test]
     fn write_test() -> Result<(), std::io::Error> {
+        let file_path = PathBuf::from("testfile.md");
+        let mut hash_map = HashMap::new();
+        hash_map.insert(file_path.clone() ,SingleFile::new(file_path.to_str().unwrap().to_owned()));
         Writer::save(
             PathBuf::from("test/test.json"),
-            RifList{files: vec![ SingleFile::new("testfile.md") ]}
-        )
+            RifList{files: hash_map}
+        )?;
+        Ok(())
     }
 
     fn read_test() -> Result<(), std::io::Error> {
