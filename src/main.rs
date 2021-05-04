1e50b234fdb24451a265bc17c606a0999a320170
simoncreek <simoncreek@tutanota.com>
2021-05-05 02:47:04 +0900

    Experimenting several changes

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,25 @@
 mod models;
 mod process;
 mod test;
+mod utils;
 
 use std::path::{PathBuf};
-use models::{RifList, RifError};
+use models::{RifList, RifError, FileStatus};
 use process::Reader;
 
 fn main() -> Result<(), RifError> {
+
     let mut rif_list = Reader::read(
         PathBuf::from("test/test.json")
     )?;
-    println!("{:#?}", rif_list);
 
-    rif_list.add_file(PathBuf::from("test/new.md"))?;
+    let path = PathBuf::from("test/new.md");
+
+    rif_list.add_file(&path)?;
+    rif_list.add_reference(&path, &PathBuf::from("test/test.json"))?;
     println!("{:#?}", rif_list);
+    //rif_list.set_file_status(&path, FileStatus::Stale)?;
+    //rif_list.remove_file(&path)?;
 
     Ok(())
 }
