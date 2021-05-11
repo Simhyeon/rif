f13d2005740c2af2594e8cbd25a3d790ac904ed8
simoncreek <simoncreek@tutanota.com>
2021-05-12 01:23:47 +0900

    Added checker test

--- src/test.rs
+++ src/test.rs
@@ -2,11 +2,24 @@
 mod test {
     use crate::process::Reader;
     use crate::process::Writer;
-    use crate::models::{ RifList ,SingleFile};
+    use crate::checker::*;
+    use crate::models::{ RifList ,SingleFile, RifError};
     use std::path::PathBuf;
     use std::collections::HashMap;
 
     #[test]
+    fn check_test() -> Result<(), RifError> {
+        let mut rif_list = Reader::read(
+            PathBuf::from("test/test.json")
+        )?;
+
+        let mut checker = Checker::new();
+        checker.add_rif_list(&rif_list)?;
+        checker.check(&mut rif_list)?;
+
+        Ok(())
+    }
+
     fn write_test() -> Result<(), std::io::Error> {
         let file_path = PathBuf::from("testfile.md");
         let mut hash_map = HashMap::new();
