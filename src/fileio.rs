415a2f7fc8cacc01d24bd0f64548c89157d70f4d
simoncreek <simoncreek@tutanota.com>
2021-05-15 02:34:58 +0900

    Added new methods
    Made two overloaded functions for read and save because pathbuf was cumbersome to create from &str

--- src/fileio.rs
+++ src/fileio.rs
@@ -6,15 +6,30 @@ pub struct FileIO;
 impl FileIO {
     // Read file and construct into RifList struct
     // Return error on errorneous process
-    pub fn read(file_name: PathBuf) -> Result<RifList, RifError> {
+    pub fn read(file_name: &PathBuf) -> Result<RifList, RifError> {
         let rif_list: RifList = serde_json::from_str(&std::fs::read_to_string(file_name)?)?;
         rif_list.sanity_check()?;
 
         println!("Successfully read file");
         Ok(rif_list)
     }
+    // Same method but gets str
+    pub fn read_with_str(file_name: &str) -> Result<RifList, RifError> {
+        let rif_list: RifList = serde_json::from_str(file_name)?;
+        rif_list.sanity_check()?;
+
+        println!("Successfully read file");
+        Ok(rif_list)
+    }
+
     // Save/Update rif list into a file
-    pub fn save(file_name: PathBuf, rif_list: RifList) -> Result<(), RifError> {
+    pub fn save(file_name: &PathBuf, rif_list: RifList) -> Result<(), RifError> {
+        let rif_content = serde_json::to_string(&rif_list)?;
+        std::fs::write(file_name, rif_content)?;
+        Ok(())
+    }
+
+    pub fn save_with_str(file_name: &str, rif_list: RifList) -> Result<(), RifError> {
         let rif_content = serde_json::to_string(&rif_list)?;
         std::fs::write(file_name, rif_content)?;
         Ok(())
