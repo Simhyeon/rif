aac0a611a6c80a561ea1b5e4cb08c271b9b5d4c3
simoncreek <simoncreek@tutanota.com>
2021-05-15 02:35:19 +0900

    Currently testing cli

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,23 @@ mod test;
 mod utils;
 mod checker;
 mod cli;
+mod tracker;
+mod consts;
 
 use std::path::{PathBuf};
 use models::{RifList, RifError, FileStatus};
 use checker::Checker;
 use fileio::FileIO;
+use cli::Cli;
 
 fn main() -> Result<(), RifError> {
 
-    let mut rif_list = FileIO::read(
-        PathBuf::from("test/test.json")
-    )?;
+    println!("{:#?}", std::env::current_dir()?);
+    Cli::parse()?;
+
+    //let mut rif_list = FileIO::read(
+        //PathBuf::from("test/test.json")
+    //)?;
     
     //let mut checker = Checker::new();
     //checker.add_rif_list(&rif_list)?;
