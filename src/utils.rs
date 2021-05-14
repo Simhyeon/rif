ae4730edd45260615a1d5378e4e688e199865097
simoncreek <simoncreek@tutanota.com>
2021-05-15 02:33:20 +0900

    Changed method + new method
    Made get_file_unix_time return result rather than option
    Made get_current_unix_time method

--- src/utils.rs
+++ src/utils.rs
@@ -1,18 +1,21 @@
 use std::path::PathBuf;
 use chrono::NaiveDateTime;
 use filetime::FileTime;
+use crate::models::RifError;
 
 // Get file name and return filestamp as unix time(Epoch time)
-pub fn get_file_unix_time(path: PathBuf) -> Option<NaiveDateTime> {
-    let metadata = 
-        if let Ok(data) = std::fs::metadata(&path) {
-            data
-        } else {
-            return None;
-        };
-    // Get last modification time 
+pub fn get_file_unix_time(path: &PathBuf) -> Result<NaiveDateTime, RifError> {
+    let metadata = std::fs::metadata(&path)?;
+    // File
     let mtime = FileTime::from_last_modification_time(&metadata);
     // Convert to unix_time
     let unix_time = chrono::NaiveDateTime::from_timestamp(mtime.unix_seconds(), 0);
-    Some(unix_time)
+    Ok(unix_time)
+}
+
+pub fn get_current_unix_time() -> NaiveDateTime {
+    let now = chrono::Utc::now().timestamp();
+    let unix_time = chrono::NaiveDateTime::from_timestamp(now, 0);
+
+    unix_time
 }
