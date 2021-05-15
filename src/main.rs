2f532971558c02bebda9d9e1b5c39d4d87dd1bfb
simoncreek <simoncreek@tutanota.com>
2021-05-15 21:25:11 +0900

    Removed unused imports

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,16 @@ mod test;
 mod utils;
 mod checker;
 mod cli;
-mod tracker;
 mod consts;
 
-use std::path::{PathBuf};
-use models::{RifList, RifError, FileStatus};
-use checker::Checker;
-use fileio::FileIO;
+//use std::path::{PathBuf};
+use models::RifError;
+//use checker::Checker;
+//use fileio::FileIO;
 use cli::Cli;
 
 fn main() -> Result<(), RifError> {
 
-    println!("{:#?}", std::env::current_dir()?);
     Cli::parse()?;
 
     //let mut rif_list = FileIO::read(
