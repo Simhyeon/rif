a718dac3c5a472b44b0108f916848684a00a8a6f
simoncreek <simoncreek@tutanota.com>
2021-05-18 15:31:54 +0900

    Changed public method to read_black_list
    Reading raw rifignore was shady and clunky made safe wrapper around rifignore file that automatically includes consts files

--- src/fileio/etc_io.rs
+++ src/fileio/etc_io.rs
@@ -2,13 +2,27 @@ use std::path::PathBuf;
 use crate::models::rif_error::RifError;
 use std::collections::HashSet;
 use crate::consts::*;
+use crate::utils;
 use std::{
     fs::File,
     io::{prelude::*, BufReader},
 };
 
+pub fn read_black_list() -> Result<HashSet<PathBuf>, RifError> {
 
-pub fn read_rif_ignore() -> Result<HashSet<PathBuf>, RifError> {
+    let mut black_list: HashSet<PathBuf> = HashSet::new();
+
+    // Const files that should be ignored
+    black_list.extend(BLACK_LIST.to_vec().iter().map(|a| PathBuf::from(*a)).collect::<HashSet<PathBuf>>());
+
+    // Rif ignore files
+    let rif_ignore = read_rif_ignore()?;
+    black_list.extend(rif_ignore);
+
+    Ok(black_list)
+}
+
+fn read_rif_ignore() -> Result<HashSet<PathBuf>, RifError> {
     if let Ok(file) = File::open(RIF_IGNORE_FILE) {
         let buffer = BufReader::new(file);
         let ignore: Result<HashSet<PathBuf>, RifError> = buffer
