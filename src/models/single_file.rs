0dab9a18c8c20b44a009b8c4926b3461d545a25e
simoncreek <simoncreek@tutanota.com>
2021-05-19 23:12:52 +0900

    Added rustdoc + changed SingleFile's name variable to hold file name not full path

--- src/models/single_file.rs
+++ src/models/single_file.rs
@@ -6,13 +6,22 @@ use serde::{ Serialize, Deserialize };
 use crate::models::enums::FileStatus;
 use crate::utils;
 
+/// Struct that contains information about single file in rif
 #[derive(Serialize, Deserialize, Debug)]
 pub struct SingleFile {
+    /// Name of the file, it is not full path
     name: String,
+    /// Current status of the file
     pub status: FileStatus,
-    // Important - This is a unix time represented as naive date time
+    /// Current last modified time stored in rif.
+    ///
+    /// This is not same with system's last modified time
     pub last_modified : NaiveDateTime,
+    /// Timestamp of the file
+    ///
+    /// This is a critera to compare file status
     pub timestamp: NaiveDateTime,
+    /// Files set that contains referencing files
     pub references: HashSet<PathBuf>,
 }
 
@@ -20,7 +29,7 @@ impl SingleFile {
     // Mostly for debugging purpose
     pub fn new(name: PathBuf) -> Self {
         Self {  
-            name : name.to_str().unwrap().to_owned(),
+            name : name.file_name().unwrap().to_str().unwrap().to_owned(),
             status: FileStatus::Fresh,
             last_modified: utils::get_current_unix_time(),
             timestamp: utils::get_current_unix_time(),
