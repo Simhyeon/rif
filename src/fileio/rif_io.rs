d96b33080a2b68c694d0658b5bc5bddf8fa2af46
simoncreek <simoncreek@tutanota.com>
2021-05-19 23:12:16 +0900

    Added rustdoc + removed unnecessary complication and methods

--- src/fileio/rif_io.rs
+++ src/fileio/rif_io.rs
@@ -1,39 +1,25 @@
-use std::path::PathBuf;
-
+use crate::consts::*;
 use crate::models::{
     rif_error::RifError,
     rif_list::RifList, 
 };
 
-// Read file and construct into RifList struct
-// Return error on errorneous process
-pub fn read(file_name: &PathBuf) -> Result<RifList, RifError> {
-    let rif_list: RifList = serde_json::from_str(&std::fs::read_to_string(file_name)?)?;
-    rif_list.sanity_check()?;
-    Ok(rif_list)
-}
-// Same method but gets str
-pub fn read_with_str(file_name: &str) -> Result<RifList, RifError> {
-    let rif_list: RifList = serde_json::from_str(&std::fs::read_to_string(file_name)?)?;
+/// Read rif file and return rif list
+pub fn read() -> Result<RifList, RifError> {
+    let rif_list: RifList = serde_json::from_str(&std::fs::read_to_string(RIF_LIST_FILE)?)?;
     rif_list.sanity_check()?;
     Ok(rif_list)
 }
 
-// This method doesn't check sanity of file
-pub fn read_with_str_as_raw(file_name: &str) -> Result<RifList, RifError> {
-    let rif_list: RifList = serde_json::from_str(&std::fs::read_to_string(file_name)?)?;
+/// Read rif file without sanity check
+pub fn read_as_raw() -> Result<RifList, RifError> {
+    let rif_list: RifList = serde_json::from_str(&std::fs::read_to_string(RIF_LIST_FILE)?)?;
     Ok(rif_list)
 }
 
-// Save/Update rif list into a file
-pub fn save(file_name: &PathBuf, rif_list: RifList) -> Result<(), RifError> {
+/// Save rif list into rif file
+pub fn save(rif_list: RifList) -> Result<(), RifError> {
     let rif_content = serde_json::to_string(&rif_list)?;
-    std::fs::write(file_name, rif_content)?;
-    Ok(())
-}
-
-pub fn save_with_str(file_name: &str, rif_list: RifList) -> Result<(), RifError> {
-    let rif_content = serde_json::to_string(&rif_list)?;
-    std::fs::write(file_name, rif_content)?;
+    std::fs::write(RIF_LIST_FILE, rif_content)?;
     Ok(())
 }
