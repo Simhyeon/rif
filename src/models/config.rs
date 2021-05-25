45bec9bc2483f610dfc01fcb1f2f91824869d415
simoncreek <simoncreek@tutanota.com>
2021-05-25 15:54:38 +0900

    Added history capacity + Added io method

--- src/models/config.rs
+++ src/models/config.rs
@@ -2,11 +2,35 @@ use std::process::Command;
 
 use serde::{ Deserialize, Serialize };
 
+use crate::consts::*;
+use crate::models::rif_error::RifError;
+
 #[derive(Deserialize, Serialize, Debug)]
 pub struct Config{ 
-    // History Capacity
+    pub histoy_capacity: usize,
     // Check hook status
     // Check hook script 
     // Argument options(Which argument to pass) -> Only get fresh vector, Only get stale vector, Get json struct
     // Set check after update, or --check flag as default
+}
+
+impl Config {
+    pub fn new() -> Self {
+        Self {
+            histoy_capacity: 30,
+        }
+    }
+
+    /// Read config from a file
+    pub fn read_from_file() -> Result<Self, RifError> {
+        Ok(serde_json::from_str(&std::fs::read_to_string(RIF_CONFIG)?)?)
+    }
+
+    /// Save config into a file
+    pub fn save_to_file(&self) -> Result<(), RifError> {
+        let rif_config = serde_json::to_string(self)?;
+        std::fs::write(RIF_CONFIG, rif_config)?;
+
+        Ok(())
+    }
 }
