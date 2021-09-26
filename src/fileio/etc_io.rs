549f8c5d0a6e4b073e94b65714ca2ed124a707b2
simoncreek <simoncreek@tutanota.com>
2021-09-27 03:28:36 +0900

    Added support for gitignore

--- src/fileio/etc_io.rs
+++ src/fileio/etc_io.rs
@@ -11,7 +11,7 @@ use crate::models::rif_error::RifError;
 /// Get black list
 ///
 /// This read rig ignore file contents and merge with const black list contents
-pub fn get_black_list() -> Result<HashSet<PathBuf>, RifError> {
+pub fn get_black_list(use_gitignore: bool) -> Result<HashSet<PathBuf>, RifError> {
     let mut black_list: HashSet<PathBuf> = HashSet::new();
     let rif_ignore = read_rif_ignore()?;
 
@@ -19,6 +19,11 @@ pub fn get_black_list() -> Result<HashSet<PathBuf>, RifError> {
     black_list.extend(BLACK_LIST.to_vec().iter().map(|a| PathBuf::from(*a)).collect::<HashSet<PathBuf>>());
     // Rif ignore files
     black_list.extend(rif_ignore);
+
+    // Include git ignore files
+    if use_gitignore{
+        black_list.extend(read_git_ignore()?);
+    } 
 
     Ok(black_list)
 }
@@ -37,6 +42,22 @@ fn read_rif_ignore() -> Result<HashSet<PathBuf>, RifError> {
         Ok(ignore?)
     } else {
         // It is perfectly normal that rifignore file doesn't exist
+        Ok(HashSet::new())
+    }
+}
+
+/// Read git ignore file
+fn read_git_ignore() -> Result<HashSet<PathBuf>, RifError> {
+    if let Ok(file) = File::open(".gitignore") {
+        let buffer = BufReader::new(file);
+        let ignore: Result<HashSet<PathBuf>, RifError> = buffer
+            .lines()
+            .map(|op| -> Result<PathBuf, RifError> {Ok(PathBuf::from(op?))})
+            .collect();
+
+        Ok(ignore?)
+    } else {
+        // It is perfectly normal that gitignore file doesn't exist
         Ok(HashSet::new())
     }
 }
