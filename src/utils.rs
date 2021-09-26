f8d659005c17bc6f5221b9d2abf9efe73de47488
simoncreek <simoncreek@tutanota.com>
2021-09-27 04:58:40 +0900

    Reloated io methods into utils

--- src/utils.rs
+++ src/utils.rs
@@ -1,10 +1,13 @@
+use std::io::{BufRead, BufReader};
+use std::fs::File;
 use std::fs::metadata;
+use std::collections::HashSet;
 use std::path::{PathBuf, Path};
 
 use chrono::NaiveDateTime;
 use filetime::FileTime;
 use crate::consts::*;
-use crate::models::rif_error::RifError;
+use crate::RifError;
 
 /// Get file's system timestamp in unix time
 ///
@@ -148,8 +151,8 @@ pub fn relativize_path(path: &Path) -> Result<PathBuf, RifError> {
 ///
 /// Return error if rif file is not in current working directory
 pub fn check_rif_file() -> Result<(), RifError> {
-    if !PathBuf::from(RIF_LIST_FILE).exists() {
-        return Err(RifError::RifIoError(format!("\"{}\" doesn't exist in current working directory", RIF_LIST_FILE)));
+    if !PathBuf::from(RIF_REL_FILE).exists() {
+        return Err(RifError::RifIoError(format!("\"{}\" doesn't exist in current working directory", RIF_REL_FILE)));
     }
     Ok(())
 }
@@ -160,4 +163,58 @@ pub fn check_rif_file() -> Result<(), RifError> {
 pub enum LoopBranch {
     Exit,
     Continue,
+}
+
+/// Get black list
+///
+/// This read rig ignore file contents and merge with const black list contents
+pub fn get_black_list(use_gitignore: bool) -> Result<HashSet<PathBuf>, RifError> {
+    let mut black_list: HashSet<PathBuf> = HashSet::new();
+    let rif_ignore = read_rif_ignore()?;
+
+    // Const files that should be ignored
+    black_list.extend(BLACK_LIST.to_vec().iter().map(|a| PathBuf::from(*a)).collect::<HashSet<PathBuf>>());
+    // Rif ignore files
+    black_list.extend(rif_ignore);
+
+    // Include git ignore files
+    if use_gitignore{
+        black_list.extend(read_git_ignore()?);
+    } 
+
+    Ok(black_list)
+}
+
+/// Read rif ignore file
+///
+/// Do io operation and converts into hashset
+fn read_rif_ignore() -> Result<HashSet<PathBuf>, RifError> {
+    if let Ok(file) = File::open(RIF_IGNORE_FILE) {
+        let buffer = BufReader::new(file);
+        let ignore: Result<HashSet<PathBuf>, RifError> = buffer
+            .lines()
+            .map(|op| -> Result<PathBuf, RifError> {Ok(PathBuf::from(op?))})
+            .collect();
+
+        Ok(ignore?)
+    } else {
+        // It is perfectly normal that rifignore file doesn't exist
+        Ok(HashSet::new())
+    }
+}
+
+/// Read git ignore file
+fn read_git_ignore() -> Result<HashSet<PathBuf>, RifError> {
+    if let Ok(file) = File::open(".gitignore") {
+        let buffer = BufReader::new(file);
+        let ignore: Result<HashSet<PathBuf>, RifError> = buffer
+            .lines()
+            .map(|op| -> Result<PathBuf, RifError> {Ok(PathBuf::from(op?))})
+            .collect();
+
+        Ok(ignore?)
+    } else {
+        // It is perfectly normal that gitignore file doesn't exist
+        Ok(HashSet::new())
+    }
 }
