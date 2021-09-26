d066fb8fe4f6ea3c9a563d2348a89239f1e5b603
simoncreek <simoncreek@tutanota.com>
2021-09-27 03:23:49 +0900

    Removed capacity + Changed from json format to binary format

--- src/models/history.rs
+++ src/models/history.rs
@@ -3,7 +3,6 @@ use std::path::PathBuf;
 use std::collections::HashMap;
 use crate::consts::*;
 use crate::models::rif_error::RifError;
-use crate::models::config::Config;
 use colored::*;
 
 /// Struct history of rif update messags
@@ -23,15 +22,26 @@ impl History {
 
     /// Read history struct from a file
     pub fn read_from_file() -> Result<Self, RifError> {
-        Ok(serde_json::from_str(&std::fs::read_to_string(RIF_UPDATE_HISTORY)?)?)
+        let result = bincode::deserialize::<Self>(&std::fs::read(RIF_UPDATE_HISTORY)?);
+        match result {
+            Err(err) => {
+                Err(RifError::BincodeError(err))
+            }
+            Ok(history) => {
+                Ok(history)
+            }
+        }
     }
 
     /// Save history struct into a file
     pub fn save_to_file(&self) -> Result<(), RifError> {
-        let rif_history = serde_json::to_string(self)?;
-        std::fs::write(RIF_UPDATE_HISTORY, rif_history)?;
-
-        Ok(())
+        let result = bincode::serialize(self);
+        if let Err(err) = result {
+            Err(RifError::BincodeError(err))
+        } else {
+            std::fs::write(RIF_UPDATE_HISTORY, result.unwrap())?;
+            Ok(())
+        }
     }
 
     /// Add new history
@@ -41,21 +51,10 @@ impl History {
     ///
     /// * `path` - Target file name
     /// * `msg` - Message to add
-    pub fn add_history(&mut self, path: &PathBuf, msg: &str, config: &Config) -> Result<(), RifError> {
+    pub fn add_history(&mut self, path: &PathBuf, msg: &str) -> Result<(), RifError> {
         // Whether history already exists or not
         if let Some(unit) = self.hist_map.get_mut(path) {
-            if config.histoy_capacity != 0 {
-                unit.push(msg.to_owned());
-            } else {
-                return Err(RifError::ConfigError(String::from("Update message is not added because capacity is 0")));
-            }
-
-            while unit.len() > config.histoy_capacity {
-                println!("Removing LEN : {} CAP : {}", unit.len(), config.histoy_capacity);
-                // NOTE ::: Not so sure if this is really efficient than simple remove(0)
-                unit.rotate_left(1);
-                unit.pop();
-            }
+            unit.push(msg.to_owned());
         } else {
             self.hist_map.insert(path.to_path_buf(), vec![msg.to_owned()]);
         }
