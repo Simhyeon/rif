171cafe763a94e4a56331ad163ff0cedf9ecd12c
simoncreek <simoncreek@tutanota.com>
2021-09-28 04:37:02 +0900

    Renamed main file

--- src/bin.rs
+++ src/bin.rs
@@ -1,10 +1,13 @@
-use rif::cli::Cli;
+#[cfg(feature = "clap")]
+use rif::Cli;
 use rif::RifError;
 
 fn main() -> Result<(), RifError> {
 
     // Enable color on pager such as "less"
+    #[cfg(feature = "color")]
     colored::control::set_override(true);
+    #[cfg(feature = "clap")]
     if let Err(error_content) =  Cli::parse() {
         println!("{}", error_content);
     }
