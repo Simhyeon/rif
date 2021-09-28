23b047d3c777d3d06e560290c4c32a9e2289a2af
simoncreek <simoncreek@tutanota.com>
2021-09-28 22:14:58 +0900

    Refactoring

--- src/rif/meta.rs
+++ src/rif/meta.rs
@@ -7,16 +7,40 @@ use crate::utils;
 /// Meta information related to rif directory
 #[derive(Serialize, Deserialize, Debug)]
 pub(crate) struct Meta {
-    pub to_be_forced: HashSet<PathBuf>,
-    pub to_be_added: HashSet<PathBuf>,
+    to_be_forced: HashSet<PathBuf>,
+    to_be_added: HashSet<PathBuf>,
 }
 
-impl Meta {
+impl<'a> Meta {
     pub fn new() -> Self {
         Self {
             to_be_added: HashSet::new(),
             to_be_forced: HashSet::new(),
         }
+    }
+
+    pub fn add(&mut self, file : &Path, force: bool) {
+        if !self.to_be_added.contains(file) && !self.to_be_forced.contains(file) {
+            if force {
+                self.to_be_forced.insert(file.to_owned());
+            } else {
+                self.to_be_added.insert(file.to_owned());
+            }
+        }
+    }
+
+    pub fn remove(&mut self, file : &Path, force: bool) {
+        if self.to_be_added.contains(file) || self.to_be_forced.contains(file) {
+            if force {
+                self.to_be_forced.remove(file);
+            } else {
+                self.to_be_added.remove(file);
+            }
+        }
+    }
+
+    pub fn to_be_added_later(&'a self) -> impl Iterator<Item = &PathBuf> + 'a {
+        self.to_be_added.iter().chain(self.to_be_forced.iter())
     }
 
     pub fn read_from_file(path: Option<impl AsRef<Path>>) -> Result<Self, RifError> {
